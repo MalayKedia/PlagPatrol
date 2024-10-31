ff43d68416faee9f7079c016c5f31b7c0b6e5542
MalayKedia <malay.kedia@gmail.com>
2024-10-31 14:36:44 +0530

    fixed exact matches

--- phase1/match_submissions.hpp
+++ phase1/match_submissions.hpp
@@ -10,75 +10,9 @@
 // Also DO NOT add the include "bits/stdc++.h"
 
 // OPTIONAL: Add your helper functions and data structures here
-#include <stdlib.h>
-#include <string.h> 
-#include <limits.h>
-
- 
- const char *bitap_fuzzy_bitwise_search(const vector<int>& text, const vector<int>& pattern, int k)
- {
-     const char *result = NULL;
-     int m = strlen(pattern);
-     unsigned long *R;
-     unsigned long pattern_mask[CHAR_MAX+1];
-     int i, d;
- 
-     if (pattern[0] == '\0') return text;
-     if (m > 31) return "The pattern is too long!";
- 
-     /* Initialize the bit array R */
-     R = malloc((k+1) * sizeof *R);
-     for (i=0; i <= k; ++i)
-         R[i] = ~1;
- 
-     /* Initialize the pattern bitmasks */
-     for (i=0; i <= CHAR_MAX; ++i)
-         pattern_mask[i] = ~0;
-     for (i=0; i < m; ++i)
-         pattern_mask[pattern[i]] &= ~(1UL << i);
- 
-     for (i=0; text[i] != '\0'; ++i) {
-         /* Update the bit arrays */
-         unsigned long old_Rd1 = R[0];
- 
-         R[0] |= pattern_mask[text[i]];
-         R[0] <<= 1;
- 
-         for (d=1; d <= k; ++d) {
-             unsigned long tmp = R[d];
-             /* Substitution is all we care about */
-             R[d] = (old_Rd1 & (R[d] | pattern_mask[text[i]])) << 1;
-             old_Rd1 = tmp;
-         }
- 
-         if (0 == (R[k] & (1UL << m))) {
-             result = (text+i - m) + 1;
-             break;
-         }
-     }
- 
-     free(R);
-     return result;
- }
-
-
-int fuzzy_matches(const std::vector<int>& vec1, const std::vector<int>& vec2, int minLength, int err){
-    int len1 = vec1.size();
-    int len2 = vec2.size();
-    int count = 0;
-
-    for(int i=0; i<len1; i++){
-        for(int j=0; j<len2; j++){
-            for(int k=30; k<32; k++){
-                if(bitap_fuzzy_bitwise_search())
-            }
-        }
-    }
-}
-
 
 // Function to find matching subsequences
-int LengthOfMatchingSubsequences(const std::vector<int>& vec1, const std::vector<int>& vec2, int minLength) {
+int LengthOfExactMatch(const std::vector<int>& vec1, const std::vector<int>& vec2, int minLength) {
     int len1 = vec1.size();
     int len2 = vec2.size();
     int count = 0;
@@ -87,7 +21,7 @@ int LengthOfMatchingSubsequences(const std::vector<int>& vec1, const std::vector
     for(int i=0; i<len1; i++){
         int match = 0;
         for(int j=0; j<len2; j++){
-            if(i>=1 && j>=1 && vec1[i-1]==vec2[j-1]) break;
+            // if(i>=1 && j>=1 && vec1[i-1]==vec2[j-1]) break;
             int k;
             for(k=0; j+k<len2 && i+k<len1; k++){
                 if(vec1[i+k]!=vec2[j+k]) break;
@@ -96,14 +30,12 @@ int LengthOfMatchingSubsequences(const std::vector<int>& vec1, const std::vector
                 match = k;
             }
         }
-        // if(match>=minLength && match>count) {
-        //     count = match;
-        //     std::cout<<i<<" match "<<match<<"\n";
-        // }      // use the maximum length of exact match
+
         if(match>=minLength) {
             count += match;
-            std::cout<<i<<" match "<<match<<"\n";
-        }         // use the total length of exact matches
+            i+=match-1;
+            std::cerr<<"At "<<i<<" match of len "<<match<<"\n";
+        }     
     }
 
     return count;
@@ -113,7 +45,7 @@ std::array<int, 5> match_submissions(std::vector<int> &submission1, std::vector<
     // TODO: Write your code here
     std::array<int, 5> result = {0, 0, 0, 0, 0};
 
-    result[1] = LengthOfMatchingSubsequences(submission1, submission2, 10);
+    result[1] = LengthOfExactMatch(submission1, submission2, 10);
     // result[2] = bitap_fuzzy_bitwise_search()
     // result[0] = (result[1]>0);
 
