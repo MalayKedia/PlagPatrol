04d481461f6a03cac689e60cde2232e2801fc8c4
MalayKedia <malay.kedia@gmail.com>
2024-11-25 13:23:05 +0530

    phase 3 working dir

---
+++ phase3/body.cpp
@@ -0,0 +1,34 @@
+#include "../tokenizer.hpp"
+#include <iomanip>
+
+// You should NOT modify ANYTHING in this file.
+extern std::array<int, 5> match_submissions(std::vector<int> &submission1, 
+        std::vector<int> &submission2);
+
+void execute_and_verify_testcase(std::string test_dir) {
+    tokenizer_t file_one(test_dir + "/one.cpp");
+    tokenizer_t file_two(test_dir + "/two.cpp");
+    std::vector<int> submission1 = file_one.get_tokens();
+    std::vector<int> submission2 = file_two.get_tokens();
+    std::array<int, 5> output = match_submissions(submission1, submission2);
+
+    for(int i = 0; i < 5; i++) {
+		std::cout<<output[i]<<" ";
+    }
+	std::cout<<std::endl;
+}
+
+int main(void) {
+
+    std::cout<<"Testcase 1: ";
+    execute_and_verify_testcase("testcases/one");
+
+    std::cout<<"Testcase 2: ";
+    execute_and_verify_testcase("testcases/two");
+
+    std::cout<<"Testcase 3: ";
+    execute_and_verify_testcase("testcases/three");
+
+    std::cout<<std::endl;
+    return 0;
+}
