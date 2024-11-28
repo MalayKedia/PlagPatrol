bbf37835d9e5a029f560590c127544154ea8041e
Jigyasa Chouhan <jigyasachouhan@Jigyasas-MacBook-Air.local>
2024-11-28 22:15:34 +0530

    phase3 testcases

--- phase3/body.cpp
+++ phase3/body.cpp
@@ -20,14 +20,26 @@ void execute_and_verify_testcase(std::string test_dir) {
 
 int main(void) {
 
-    std::cout<<"Testcase 1: ";
-    execute_and_verify_testcase("testcases/one");
+    // std::cout<<"Testcase 1: ";
+    // execute_and_verify_testcase("testcases/one");
 
-    std::cout<<"Testcase 2: ";
-    execute_and_verify_testcase("testcases/two");
+    // std::cout<<"Testcase 2: ";
+    // execute_and_verify_testcase("testcases/two");
 
-    std::cout<<"Testcase 3: ";
-    execute_and_verify_testcase("testcases/three");
+    // std::cout<<"Testcase 3: ";
+    // execute_and_verify_testcase("testcases/three");
+
+    std::cout<<"Test 1: ";
+    execute_and_verify_testcase("testcases/test1");
+
+    std::cout<<"Test 2: ";
+    execute_and_verify_testcase("testcases/test2");
+
+    std::cout<<"Test 3: ";
+    execute_and_verify_testcase("testcases/test3");
+    
+    std::cout<<"Test 4: ";
+    execute_and_verify_testcase("testcases/test4");
 
     std::cout<<std::endl;
     return 0;
